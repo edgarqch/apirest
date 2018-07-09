b54a13e7aa88bf8e047cbb91773da5be93861c8b
Edgar Quispe <edgar31641@gmail.com>
2018-07-09 18:03:29 -0400

    service IMC created

---
+++ routes/api/v1.0/service.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/imc', function(req, res, next) {
+    var imc = Number(req.body.masa) / Math.pow(Number(req.body.altura), 2)
+    if (imc < 16) {
+        res.send({
+            "msn": "Delgadez moderada"
+        });
+    } else if (imc > 17 && imc < 18.49) {
+        res.send({
+            "msn": "Delgadez leve"
+        });
+    } else if (imc >= 18.5 && imc <= 24.99) {
+        res.send({
+            "msn": "Normal"
+        });
+    } else if (imc >= 25 && imc <= 29.99) {
+        res.send({
+            "msn": "Sobre Peso"
+        });
+    } else if (imc >= 30 && imc <= 39.99) {
+        res.send({
+            "msn": "Obesidad"
+        });
+    } else if (imc >= 40) {
+        res.send({
+            "msn": "Obesidad Morvida"
+        });
+    } else {
+        res.send({
+            "msn": "Error en los datos"
+        });
+    }
+    //res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
